e22cb723ceb8a884ce190d5cc2f08185011335d1
Scott Carpenter <carpenter.scott22@gmail.com>
2022-12-14 00:27:46 -0600

    added JS functionality for form reset & delete

--- js/index.js
+++ js/index.js
@@ -4,25 +4,34 @@ document.querySelector('#generate-meme').addEventListener('click', (e) => {
     let bottomText = document.querySelector('#meme-bottom-text').value;
 
     const meme = `
-    <div id="meme-container" class="container d-flex justify-content-center" style="position: relative; color: #000; font-size: 36pt; font-weight: 700"> 
+    <div id="meme" class="container d-flex justify-content-center" style="position: relative; color: #000; font-size: 36pt; font-weight: 700"> 
         <div id="meme-img">
             <img src="${memeURL}">
         </div>
         <div id="top-text-container" style="position: absolute; top: 16px">
             ${topText}
         </div> 
+     
         <div id="bottom-text-container" style="position: absolute; bottom: 16px">
             ${bottomText}
         </div>
-    </div> 
+    </div>
     
-    <button id="delete-meme">Censor Meme</button>`
+    <div id="delete-container" class="container m-3"> 
+        <button id="delete-meme" class="btn btn-lg btn-danger" type="button">Delete Meme</button>
+    </div>
+    `;
 
     document.querySelector('#meme-container').insertAdjacentHTML("beforeend", meme);
     
+    document.querySelector('#delete-meme').addEventListener('click', (e) => {
+        e.preventDefault();
+        const deleteMeme = document.querySelector("#meme");
+        const deleteBtn = document.querySelector("#delete-container");
+        deleteMeme.remove();
+        deleteBtn.remove();
+        document.querySelector("#meme-form").reset();
+    });
 
 });
 
-document.querySelector('#delete-meme').addEventListener('click', (e) => {
-    meme.remove();
-});
